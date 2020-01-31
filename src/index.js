c362dc95fd5784f018c5f6ca65a3c24ffd39e814
Leorge Cabrisas <leorge.cabrisas@lmrc9467.rcoffice.ringcentral.com>
2020-01-31 11:11:33 -0500

    Add configuration

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+ReactDom.render(
+    <h2> Hello react with JXS!!</h2>,
+    // React.createElement('h2', null, 'Hello React'),
+    document.getElementById('root')
+);
