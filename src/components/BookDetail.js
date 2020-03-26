dcfb1cfd14afd71e2919650786cff761fa0dea22
Leorge Cabrisas <leorge.cabrisas@LMRC9467.local>
2020-03-26 19:09:42 -0400

    Add author’s hyperlink

--- src/components/BookDetail.js
+++ src/components/BookDetail.js
@@ -50,7 +50,7 @@ class BookDetail extends Component {
             <h5 className="card-genre">Genre: {this.state.book.genre}</h5>
             <h5 className="card-publishingInfo">Publishing Information: {this.state.book.publishingInfo.publisher}, {this.state.book.publishingInfo.publicationDate}</h5>
             <h5 className="card-genre">Genre: {this.state.book.genre}</h5>
-            <p className="card-author">{this.state.book.author}</p>
+            <p className="card-author "><strong>Author:</strong> <a className="author-link" target="_blank" href={this.state.book.moreBooks}>{this.state.book.author}</a></p>
             <p className="card-shortBio">{this.state.book.shortBio}</p>
             <Zoom>
               <img
